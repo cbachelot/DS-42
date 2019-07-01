25aae45ee87c432e1abb4543d401450809587de5
Cbachelot <clark.bachelot@alliedelec.com>
2019-07-01 15:01:14 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,31 @@
-const version = "0.6.14";
-const cacheName = `ds42-${version}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        './',
-        './ds-component-buttons.html',
-        './styles/alliedstyles.css',
-        './styles/icon-style.css',
-        './styles/slide-in-panel.css',
-        './images/ds42-mast.svg',
-        './scss/allied_variables.scss',
-        './scss/allied_variables.css',
-        './scss/ds-42.css',
-        './scss/ds-42.scss',
-        './scss/ds42_variables.css',
-        './scss/ds42_variables.scss',
-        './js/slide-in-panel.js'
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    if (event.request.mode === 'navigate') {
+      // See /web/fundamentals/getting-started/primers/async-functions
+      // for an async/await primer.
+      event.respondWith(async function() {
+        // Optional: Normalize the incoming URL by removing query parameters.
+        // Instead of https://example.com/page?key=value,
+        // use https://example.com/page when reading and writing to the cache.
+        // For static HTML documents, it's unlikely your query parameters will
+        // affect the HTML returned. But if you do use query parameters that
+        // uniquely determine your HTML, modify this code to retain them.
+        const normalizedUrl = new URL(event.request.url);
+        normalizedUrl.search = '';
+  
+        // Create promises for both the network response,
+        // and a copy of the response that can be used in the cache.
+        const fetchResponseP = fetch(normalizedUrl);
+        const fetchResponseCloneP = fetchResponseP.then(r => r.clone());
+  
+        // event.waitUntil() ensures that the service worker is kept alive
+        // long enough to complete the cache update.
+        event.waitUntil(async function() {
+          const cache = await caches.open('my-cache-name');
+          await cache.put(normalizedUrl, await fetchResponseCloneP);
+        }());
+  
+        // Prefer the cached response, falling back to the fetch response.
+        return (await caches.match(normalizedUrl)) || fetchResponseP;
+      }());
+    }
+  });

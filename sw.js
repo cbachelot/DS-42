7115bd59366e77fed90cc694437d7383a0c4185a
Cbachelot <clark.bachelot@alliedelec.com>
2019-07-01 15:03:49 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,13 @@
-self.addEventListener('fetch', event => {
-    if (event.request.mode === 'navigate') {
-      // See /web/fundamentals/getting-started/primers/async-functions
-      // for an async/await primer.
-      event.respondWith(async function() {
-        // Optional: Normalize the incoming URL by removing query parameters.
-        // Instead of https://example.com/page?key=value,
-        // use https://example.com/page when reading and writing to the cache.
-        // For static HTML documents, it's unlikely your query parameters will
-        // affect the HTML returned. But if you do use query parameters that
-        // uniquely determine your HTML, modify this code to retain them.
-        const normalizedUrl = new URL(event.request.url);
-        normalizedUrl.search = '';
-  
-        // Create promises for both the network response,
-        // and a copy of the response that can be used in the cache.
-        const fetchResponseP = fetch(normalizedUrl);
-        const fetchResponseCloneP = fetchResponseP.then(r => r.clone());
-  
-        // event.waitUntil() ensures that the service worker is kept alive
-        // long enough to complete the cache update.
-        event.waitUntil(async function() {
-          const cache = await caches.open('my-cache-name');
-          await cache.put(normalizedUrl, await fetchResponseCloneP);
-        }());
-  
-        // Prefer the cached response, falling back to the fetch response.
-        return (await caches.match(normalizedUrl)) || fetchResponseP;
-      }());
-    }
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open('mysite-dynamic').then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          var fetchPromise = fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          return response || fetchPromise;
+        })
+      })
+    );
   });

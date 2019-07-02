4f6b5d45d4250c4de31c4254a2c9b94bbfd2fd56
Cbachelot <clark.bachelot@alliedelec.com>
2019-07-02 14:41:58 -0500

    updated service worker registration

--- sw.js
+++ sw.js
@@ -4,20 +4,20 @@ self.addEventListener('install', function(event) {
       caches.open(cacheName).then(function(cache) {
         return cache.addAll(
           [
-            './',
-            './ds-component-buttons.html',
-            './styles/alliedstyles.css',
-            './styles/icon-style.css',
-            './styles/slide-in-panel.css',
-            './images/ds42-mast.svg',
-            './scss/allied_variables.scss',
-            './scss/allied_variables.css',
-            './scss/ds-42.css',
-            './scss/ds-42.scss',
-            './scss/ds42_variables.css',
-            './scss/ds42_variables.scss',
-            './js/slide-in-panel.js',
-            './nav.html'
+            '/',
+            '/ds-component-buttons.html',
+            '/styles/alliedstyles.css',
+            '/styles/icon-style.css',
+            '/styles/slide-in-panel.css',
+            '/images/ds42-mast.svg',
+            '/scss/allied_variables.scss',
+            '/scss/allied_variables.css',
+            '/scss/ds-42.css',
+            '/scss/ds-42.scss',
+            '/scss/ds42_variables.css',
+            '/scss/ds42_variables.scss',
+            '/js/slide-in-panel.js',
+            '/nav.html'
           ]
         ).then(() => self.skipWaiting());
       })

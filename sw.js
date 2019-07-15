58d18eae382f540154fb94fa49bbe84d78e449ac
Cbachelot <clark.bachelot@alliedelec.com>
2019-07-15 14:12:38 -0500

    revert sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,41 @@
-  self.addEventListener('install', e => {
-    // once the SW is installed, go ahead and fetch the resources
-    // to make this work offline
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([
-              '/',
-              '/index.html',
-              '/ds-component-buttons.html',
-              '/styles/alliedstyle.css',
-              '/styles/icon-style.css',
-              '/styles/slide-in-panel.css',
-              '/images/ds42-mast.svg',
-              '/scss/allied_variables.scss',
-              '/scss/allied_variables.css',
-              '/scss/ds-42.css',
-              '/scss/ds-42.scss',
-              '/scss/ds42_variables.css',
-              '/scss/ds42_variables.scss',
-              '/js/slide-in-panel.js',
-              '/nav.html'
-            ]).then(() => self.skipWaiting());
-        })
+const cacheName = `cacheName`;
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(
+          [
+            '/',
+            '/index.html',
+            '/ds-component-buttons.html',
+            '/styles/alliedstyle.css',
+            '/styles/icon-style.css',
+            '/styles/slide-in-panel.css',
+            '/images/ds42-mast.svg',
+            '/scss/allied_variables.scss',
+            '/scss/allied_variables.css',
+            '/scss/ds-42.css',
+            '/scss/ds-42.scss',
+            '/scss/ds42_variables.css',
+            '/scss/ds42_variables.scss',
+            '/js/slide-in-panel.js',
+            '/nav.html'
+          ]
+        ).then(() => self.skipWaiting());
+      })
     );
-});
+  });
 
-self.addEventListener('message', function (event) {
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.open(cacheName)
+        .then(cache => cache.match(event.request, {ignoreSearch: true}))
+        .then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+
+  self.addEventListener('message', function (event) {
     if (event.data.msg === 'skipwaiting') {
         self.skipWaiting();
     }

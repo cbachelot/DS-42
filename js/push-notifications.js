c947f89a837e81ac3d2ade0526d9a5c1c070a20a
Cbachelot <clark.bachelot@alliedelec.com>
2019-10-23 11:01:37 -0500

    removed push notificaitons until further testing can be done.

--- js/push-notifications.js
+++ js/push-notifications.js
@@ -1,3 +1,20 @@
 Notification.requestPermission(function(status) {
   console.log("Notification permission status:", status);
 });
+
+function displayNotification() {
+  if (Notification.permission == "granted") {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      var options = {
+        body: "Here is a notification body!",
+        icon: "../images/a-images/images/icons/icon-512x512.png",
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: 1
+        }
+      };
+      reg.showNotification("Hello world!", options);
+    });
+  }
+}
